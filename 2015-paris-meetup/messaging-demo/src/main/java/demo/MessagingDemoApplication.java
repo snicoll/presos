e1660c0b8fa936e19bc154a9a8c28794fa8a9814
Stephane Nicoll <snicoll@pivotal.io>
2014-12-29 17:50:07 +0100

    Initial import of caching demo

---
+++ 2015-paris-meetup/messaging-demo/src/main/java/demo/MessagingDemoApplication.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsMessageOperations;
+import org.springframework.messaging.support.MessageBuilder;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class MessagingDemoApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessagingDemoApplication.class);
+
+    @Autowired
+    private JmsMessageOperations messagingTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("Sending message");
+        messagingTemplate.send("smpQueue", MessageBuilder.withPayload("Hello Spring Meetup Paris!").build());
+        logger.info("Message sent successfully");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingDemoApplication.class, args);
+    }
+}
